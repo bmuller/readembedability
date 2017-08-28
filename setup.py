06324ca0bffe95bd65b0b843b476add0187ae78e
Brian Muller <bamuller@gmail.com>
2017-08-27 22:14:41 -0400

    upgrade dependencies, bump to 1.6.1

--- setup.py
+++ setup.py
@@ -13,17 +13,17 @@ setup(
     packages=find_packages(),
     package_data={'readembedability': ['data/*.*']},
     install_requires = [
-       'lxml==3.7.3',
+       'lxml==3.8.0',
        'cssselect==1.0.1',
        'readability-lxml==0.6.2',
        'robostrippy==1.3',
-       'pyOpenSSL==17.0.0',
+       'pyOpenSSL==17.2.0',
        'beautifulsoup4==4.6.0',
        'nltk==3.2.4',
        'fastimage==2.0.0',
        'pytidylib==0.3.2',
-       'newspaper3k==0.1.9',
-       'python-dateutil==2.6.0',
-       'aiohttp==2.1.0'
+       'newspaper3k==0.2.2',
+       'python-dateutil==2.6.1',
+       'aiohttp==2.2.5'
     ]
 )
